ae8b9d0f5220f327b053fe4bb7456a45dff63cb8
Sara Gfroerer <sara.gfroerer@googlemail.com>
2019-11-06 13:28:46 +0100

    new task

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
 console.log('it works')
 
+// fetch('http://localhost:3333/cards')
+//   .then(res => res.json())
+//   .then(cards => {
+//     cards.forEach(card => {
+//       const link = document.createElement('a')
+//       link.style.display = 'block'
+//       link.setAttribute('href', `http://localhost:3333/cards/${card.id}`)
+//       link.textContent = card.title
+//       document.body.appendChild(link)
+//     })
+//   })
+//   .catch(err => console.log(err))
+
 fetch('http://localhost:3333/cards')
   .then(res => res.json())
-  .then(cards => console.log(cards))
+  .then(cards => {
+    cards.forEach(card => {
+      const link = document.createElement('a')
+      link.style.display = 'block'
+      link.setAttribute('href', `http://localhost:3333/cards/${card.id}`)
+      link.textContent = card.title
+      document.body.appendChild(link)
+    })
+  })
+  .catch(err => console.log(err))
+
+// fetch('http://localhost:3333/cards')
+//   .then(res => res.json())
+//   .then(cards => cards.forEach(createLink))
+//   .catch(err => console.log('--->', err))
+// function createLink(card) {
+//   const el = document.createElement('a')
+//   el.textContent = card.title
+//   el.href = 'http://localhost:3333/cards/' + card.id
+//   el.style.display = 'block'
+//   document.body.append(el)
+// }
