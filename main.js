66a236b0dae742a13968c3d2700649e31702f674
Sara Gfroerer <sara.gfroerer@googlemail.com>
2019-11-06 14:41:47 +0100

    before cleaning code

--- main.js
+++ main.js
@@ -1,30 +1,47 @@
 console.log('it works')
 
-// fetch('http://localhost:3333/cards')
-//   .then(res => res.json())
-//   .then(cards => {
-//     cards.forEach(card => {
-//       const link = document.createElement('a')
-//       link.style.display = 'block'
-//       link.setAttribute('href', `http://localhost:3333/cards/${card.id}`)
-//       link.textContent = card.title
-//       document.body.appendChild(link)
-//     })
-//   })
-//   .catch(err => console.log(err))
-
 fetch('http://localhost:3333/cards')
   .then(res => res.json())
-  .then(cards => {
-    cards.forEach(card => {
-      const link = document.createElement('a')
-      link.style.display = 'block'
-      link.setAttribute('href', `http://localhost:3333/cards/${card.id}`)
-      link.textContent = card.title
-      document.body.appendChild(link)
+  .then(addLink)
+  .catch(err => console.log(err))
+
+function addLink(cards) {
+  cards.forEach(card => {
+    const wrapper = document.createElement('div')
+    const box = document.createElement('section')
+    const link = document.createElement('a')
+    link.href = '#'
+    link.textContent = 'show'
+    link.style.display = 'block'
+    box.classList.add('card')
+
+    wrapper.appendChild(box)
+    box.appendChild(link)
+    document.body.appendChild(wrapper)
+    const container = document.createElement('div')
+    container.classList.add('hidden')
+    container.classList.add('card')
+    const title = document.createElement('h1')
+    const question = document.createElement('h2')
+    const answer = document.createElement('p')
+
+    title.textContent = card.title
+    question.textContent = card.question
+    answer.textContent = card.answer
+
+    container.appendChild(title)
+    container.appendChild(question)
+    container.appendChild(answer)
+    wrapper.appendChild(container)
+
+    link.addEventListener('click', () => {
+      container.classList.toggle('hidden')
+      link.textContent === 'show'
+        ? (link.textContent = 'hide')
+        : (link.textContent = 'show')
     })
   })
-  .catch(err => console.log(err))
+}
 
 // fetch('http://localhost:3333/cards')
 //   .then(res => res.json())
