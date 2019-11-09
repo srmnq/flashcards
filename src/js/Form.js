da2df918c9cb138fb8f4d541f66cca20c5b4887e
Sara Gfroerer <sara.gfroerer@googlemail.com>
2019-11-09 08:22:21 +0100

    Before working form task

--- src/js/Form.js
+++ src/js/Form.js
@@ -1,5 +1,11 @@
 export default class Form {
   constructor() {
+    const templateEl = document.querySelector('#form-template')
+    const template = templateEl.innerHTML
+    document.body.insertAdjacentHTML('afterbegin', template)
     this.el = document.querySelector('.form')
+
+    
+
   }
 }
